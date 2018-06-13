c17f6d02aab759356b3338961ce6a0cb99ec6b2b
Eric Biersner <eric.biersner@gmail.com>
2018-06-13 16:50:32 -0700

    Set up configuration variables for heroku

--- config/database.js
+++ config/database.js
@@ -1,7 +1,12 @@
+const {DBUSER, DBPASSWORD, DBURL} = require('./databaseinfo.js')
+const user = process.env.DBUSER || DBUSER;
+const pass = process.env.DBPASSWORD || DBPASSWORD;
+const url = process.env.DBURL || DBURL;
 module.exports = {
 
+
     // mongoDBUrl: 'mongodb://127.0.0.1/cms'
-    mongoDBUrl: 'mongodb://cms-user:discojohn7LQZP@ds259210.mlab.com:59210/cms'
+    mongoDBUrl: `mongodb://${user}:${pass}@${url}`
 
 
 }
