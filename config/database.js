5eccd31d38045716a92ab831d2331fd718e6a205
Eric Biersner <eric.biersner@gmail.com>
2018-06-13 16:21:25 -0700

    Add database deployment

--- config/database.js
+++ config/database.js
@@ -1,6 +1,8 @@
+const {databaseUrl} = require('../databaseConfig/databaseUrl');
 module.exports = {
 
-    mongoDBUrl: 'mongodb://127.0.0.1/cms'
+    // mongoDBUrl: 'mongodb://127.0.0.1/cms'
+    mongoDBUrl: databaseUrl
 
 
 }
