2d411065f801692d812655ef80fc806dacc640ba
Eric Biersner <eric.biersner@gmail.com>
2018-06-13 16:22:14 -0700

    set up db for heroku

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const {databaseUrl} = require('../databaseConfig/databaseUrl');
 module.exports = {
 
     // mongoDBUrl: 'mongodb://127.0.0.1/cms'
-    mongoDBUrl: databaseUrl
+    mongoDBUrl: 'mongodb://cms-user:discojohn7LQZP@ds259210.mlab.com:59210/cms'
 
 
 }
